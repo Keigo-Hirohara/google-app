af5657134532c24147b8908678b0bb89ec732384
keigo-h <keigo-h@weseek.co.jp>
2022-09-01 08:32:12 +0900

    fix type error

--- components/User.tsx
+++ components/User.tsx
@@ -7,14 +7,14 @@ const User = ({className}: any) => {
   if (session) {
     return (
       <>
-      <img className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`} onClick={signOut} alt="user-image" src={session.user?.image}/>
+      <img className={`h-10 w-10 rounded-full hover:bg-gray-200 cursor-pointer p-1 ${className}`} onClick={() => signOut()} alt="user-image" src={session.user == null ? '' : session.user.image}/>
       </>
     )
   }
 
   return (
     <>
-    <button className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`} onClick={signIn}>Sign in</button>
+    <button className={`bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md ${className}`} onClick={() => signIn()}>Sign in</button>
     </>
   );
 };
